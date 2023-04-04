9b16c4269080cabcceb6d88e18ccc8223f77958a
Nicolas <nicolas.jorge@mi.unc.edu.ar>
2023-04-03 23:27:53 -0300

    Hacemos el ejercicio de watermelon y longwords

--- ICPC/palabras.cpp
+++ ICPC/palabras.cpp
@@ -5,10 +5,20 @@ int main(){
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i+=) {
+    for (int i = 0; i < n; i++) {
         string palabra;
         cin >> palabra;
 
-        int largo = palabra.
+        int largo = palabra.length();
+
+        // aca hay que comprimir
+        if (largo > 10){
+            cout << palabra[0] << largo-2 << palabra[largo-1] << endl;
+        }
+
+        //aca hay que dejar como esta
+        else {
+            cout << palabra << endl;
+        }
     }
 }
