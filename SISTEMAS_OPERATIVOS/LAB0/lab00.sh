5a30de3e37fb6881d53b0192d88d05d12a32fcf3
Nicolas Jorge <nicolas.jorge@mi.unc.edu.ar>
2024-08-22 16:12:36 -0300

    Agregamos SISTEMAS OPERATIVOS

--- SISTEMAS_OPERATIVOS/LAB0/lab00.sh
+++ SISTEMAS_OPERATIVOS/LAB0/lab00.sh
@@ -14,39 +14,8 @@ sort -k 5 -n < weather_cordoba.in > weather_sorted.txt; head -n 1 weather_sorted
 sort -k 3 -n < atpplayers.in > atpplayers_sorted.txt
 
 # Ejercicio 6
-sort -k 2 -n -r < superliga.in > superliga_sorted.txt; 
+awk -F ' ' '{print $0, $7-$8}' superliga.in | sort -k2 -k9 -n -r -t ' ' > modified_superliga.in;echo -e "${BOLD}${B_GREEN}Superliga's teams ordered were saved on $(pwd)/modified_superliga.in$RESET";echo " ";echo -e "${BOLD}${BLUE}Showing this computer's MAC Address...$RESET"
 
-awk '
-NR == 1 {
-    # Inicializa variables para la primera línea.
-    prev_points = $2;
-    prev_goal_diff = $7 - $8;
-    prev_line = $0;
-    next
-} 
-
-{
-    curr_goal_diff = $7 - $8;
-    if ($2 == prev_points) {
-        # Si los puntos son iguales, compara la diferencia de goles.
-        if (curr_goal_diff > prev_goal_diff) {
-            # Si la diferencia de goles actual es mayor, intercambia la línea actual con la anterior.
-            aux_line = prev_line;
-            prev_line = $0;
-            $0 = aux_line;
-            prev_goal_diff = curr_goal_diff;
-        } else {
-            # Si la diferencia de goles actual es menor, no hace nada y pasa a la siguiente línea.
-            next;
-        }
-    }
-}
-END {
-    # Asegúrate de imprimir la última línea procesada.
-    print prev_line;
-}' superliga_sorted.txt > superliga_final_sorted.txt
-
-# Anotacion: se podria con sort -k 2,2n -k 7,7n superliga.in > superliga_sorted.txt
 
 # Ejercicio 7
 ip link show | grep -o -P "(?<=link/ether )([0-9a-f]{2}:){5}[0-9a-f]{2}"
@@ -54,7 +23,7 @@ ip link show | grep -o -P "(?<=link/ether )([0-9a-f]{2}:){5}[0-9a-f]{2}"
 # Anotacion: (?<=link/ether ) es un lookbehind assertion en las expresiones regulares de Perl. Busca una coincidencia que esté precedida por link/ether 
 
 # Ejercicio 8
-for file in SISTEMAS_OPERATIVOS/LAB0/breaking_bad/*_es.srt; do mv "$file" "${file/_es.srt/.srt}"; done
+for file in ./breaking_bad/*_es.srt; do mv "$file" "${file/_es.srt/.srt}"; done
 
 # Anotacion: con /*_es.srt iteramos sobre todos los archivos que terminan en _es.srt.
 # ${file/_es.srt/.srt}: Usa una sustitución de cadena para reemplazar _es.srt con .srt
