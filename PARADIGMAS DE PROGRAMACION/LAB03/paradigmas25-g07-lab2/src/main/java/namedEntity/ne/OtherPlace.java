e967fc29f297d38b5f1164cd5e257adaf60d5429
Nicolas <nicolas.jorge@mi.unc.edu.ar>
2025-06-09 13:36:44 -0300

    lab02

--- PARADIGMAS DE PROGRAMACION/LAB03/paradigmas25-g07-lab2/src/main/java/namedEntity/ne/OtherPlace.java
+++
@@ -1,7 +0,0 @@
-package namedEntity.ne;
-
-public class OtherPlace extends Place {
-    public OtherPlace(String name, String canonicalForm, int frequency) {
-        super(name, canonicalForm, frequency);
-    }
-}
